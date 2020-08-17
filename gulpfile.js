fd7abf9a799f6757a6ea3793bcf1808c7d7fa96b
oksanalutsyk <oksanalutsyk95@gmail.com>
2020-08-17 22:41:48 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,14 @@ const del = require("del");
 const ts = require("gulp-typescript");
 const uglify = require("gulp-uglify");
 const strip = require('gulp-strip-comments');
+const browserify = require("browserify");
+const source = require('vinyl-source-stream');
+const tsify = require("tsify");
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
+const babelify = require('babelify');
+const glob = require('glob');
+const bro = require('gulp-bro');
 // css
 const sass = require("gulp-sass");
 const csso = require("gulp-csso");
@@ -29,18 +37,42 @@ const html = () => {
 }
 
 const scripts = () => {
-    return src("src/**/*.ts")
-        .pipe(
-            ts({
-                noImplicitAny: true,
-                experimentalDecorators: true,
-            })
-        )
+    const tsFiles = glob.sync('src/**/*.ts');
+    return browserify({ entries: tsFiles})
+        .transform("babelify", { presets: ["@babel/preset-env"] })
+        .plugin(tsify)
+        .bundle()
+        // .pipe(
+        //     ts({
+        //         noImplicitAny: true,
+        //         experimentalDecorators: true,
+        //     })
+        // )
         .pipe(concat("script.js"))
-        .pipe(uglify())
+        // .pipe(uglify())
         .pipe(strip())
+        // .pipe(source('script.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        // .pipe(sourcemaps.init({ loadMaps: true }))
+        // .pipe(sourcemaps.write('./'))
         .pipe(dest("build"));
 }
+
+// const scripts = () => {
+//     return src('src/**/*.ts')
+//         .pipe(bro({ transform: [babelify.configure({ presets: ['@babel/preset-env'] })] }))
+//         .pipe(
+//             ts({
+//                 noImplicitAny: true,
+//                 experimentalDecorators: true,
+//             })
+//         )
+//         .pipe(concat("script.js"))
+//         .pipe(strip())
+//         .pipe(uglify())
+//         .pipe(dest("build"));
+// }
 
 const scss = () => {
     return src("src/**/*.scss")
